9c4d7e843fb4b35487c14cb76d287fbd81fb0dc5
Cory Piette <coryjpiette@gmail.com>
2021-09-23 19:30:40 -0400

    adding exercises to workouts used by api

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
 
     // Used by api.js to get last workout
     app.get("/api/workouts", (req, res) => {
-        db.Workout.find({})
+          db.Workout.find({})
         .then(workout => {
             res.json(workout);
         })
@@ -12,3 +12,24 @@ module.exports = function(app) {
             res.json(err);
         });
     });
+
+
+       // Creates a new workout
+
+       app.post("/api/workouts", async (req, res)=> {
+        try{
+              const response = await db.Workout.create({type: "workout"})
+            res.json(response);
+        }
+        catch(err){
+            console.log("error occurred creating a workout: ", err)
+        }
+    })
+
+       //Add exercise to a workout
+       app.put("/api/workouts/:id", ({body, params}, res) => {
+
+        
+        // console.log(body, params)
+        const workoutId = params.id;
+        let savedExercises = [];
