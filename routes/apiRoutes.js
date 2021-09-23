f27d4596cf0893dbbab0ea8559d6a32af392268a
Cory Piette <coryjpiette@gmail.com>
2021-09-23 19:33:08 -0400

    finishing  api route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -29,7 +29,44 @@ module.exports = function(app) {
        //Add exercise to a workout
        app.put("/api/workouts/:id", ({body, params}, res) => {
 
-        
+
         // console.log(body, params)
         const workoutId = params.id;
         let savedExercises = [];
+
+        // gets saved exercises 
+
+        db.Workout.find({_id: workoutId})
+            .then(dbWorkout => {
+                // console.log(dbWorkout)
+                savedExercises = dbWorkout[0].exercises;
+                res.json(dbWorkout[0].exercises);
+                let allExercises = [...savedExercises, body]
+                console.log(allExercises)
+                updateWorkout(allExercises)
+            })
+            .catch(err => {
+                res.json(err);
+            });
+
+        function updateWorkout(exercises){
+            db.Workout.findByIdAndUpdate(workoutId, {exercises: exercises}, function(err, doc){
+            if(err){
+                console.log(err)
+            }
+
+            })
+        }
+            
+    })
+
+    app.get("/api/workouts/range", (req, res) => {
+        db.Workout.find({})
+        .then(workout => {
+            res.json(workout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+    }); 
+};
