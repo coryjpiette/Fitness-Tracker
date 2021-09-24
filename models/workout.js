0a81d7d41e7d591e1645a79c5eb67bb1edbd3cee
Cory Piette <coryjpiette@gmail.com>
2021-09-23 20:00:34 -0400

    had to do an npm reinstall

--- models/workout.js
+++ models/workout.js
@@ -13,16 +13,16 @@ const workoutSchema = new Schema(
         type: {
           type: String,
           trim: true,
-          required: "Enter an exercise type"
+          required: "Please enter an exercise type"
         },
         name: {
           type: String,
           trim: true,
-          required: "Enter an exercise name"
+          required: "Please enter an exercise name"
         },
         duration: {
           type: Number,
-          required: "Enter an exercise duration in minutes"
+          required: "Enter your exercise duration (in minutes)"
         },
         weight: {
           type: Number
