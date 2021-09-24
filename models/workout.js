b1e4a62c9c396de760d1a47c74af475ff500c33b
Cory Piette <coryjpiette@gmail.com>
2021-09-23 20:49:49 -0400

    other formatting errors

--- models/workout.js
+++ models/workout.js
@@ -3,62 +3,60 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema(
-  {
-    day: {
-      type: Date,
-      default: Date.now
+    {
+        day: {
+            type: Date,
+            default: Date.now
+        },
+        exercises: [
+            {
+                type: {
+                    type: String,
+                    trim: true,
+                    required: "Please enter an exercise type"
+                },
+                name: {
+                    type: String,
+                    trim: true,
+                    required: "Please enter an exercise name"
+                },
+                duration: {
+                    type: Number,
+                    required: "Enter your exercise duration (in minutes)"
+                },
+                weight: {
+                    type: Number
+                },
+                reps: {
+                    type: Number
+                },
+                sets: {
+                    type: Number
+                },
+                distance: {
+                    type: Number
+                }
+            }
+        ]
+
     },
-    exercises: [
-      {
-        type: {
-          type: String,
-          trim: true,
-          required: "Please enter an exercise type"
-        },
-        name: {
-          type: String,
-          trim: true,
-          required: "Please enter an exercise name"
-        },
-        duration: {
-          type: Number,
-          required: "Enter your exercise duration (in minutes)"
-        },
-        weight: {
-          type: Number
-        },
-        reps: {
-          type: Number
-        },
-        sets: {
-          type: Number
-        },
-        distance: {
-          type: Number
+    {
+        toJSON: {
+            // add virtual properties when data is requested
+
+            virtuals: true
         }
-      }
-    ]
-
-
-  },
-  {
-    toJSON: {
-      // add virtual properties when data is requested
-
-      virtuals: true
     }
-  }
 
 );
-
 // adds a property to schema
 
-workoutSchema.virtual("totalDuration").get(function() {
-  //"reduce array of exercises down to just the sum of their durations
+workoutSchema.virtual("totalDuration").get(function () {
+    //"reduce array of exercises down to just the sum of their durations
 
-  return this.exercises.reduce((total, exercise) => {
-    return total + exercise.duration;
-  }, 0);
+    return this.exercises.reduce((total, exercise) => {
+        return total + exercise.duration;
+    }, 0);
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
